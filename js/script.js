5ff145930f23f50609cf2419e05ecb7c9c8e3af0
Michael Guerrero <michaelguerrero@uagateway.org>
2019-03-25 21:33:14 +0000

    Yikes Scoob!

--- js/script.js
+++ js/script.js
@@ -3,9 +3,27 @@
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
 
+
+
+
+
 $("#search-button").click(function(){
-  
-  
+
+$(".text-center").empty();
+
+
+  $.ajax({
+    
+   
+        url: "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC",
+        method: "GET",
+        success: function(response) {
+            
+             $(".text-center").append("<img src='" + response.data[0].images.original_still.url + "' </img>");
+            console.log(response.data[0].images.original_still.url);
+
+        }   
+    }); 
   
 });
 
